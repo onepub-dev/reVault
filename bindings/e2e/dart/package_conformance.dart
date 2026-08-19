a9abe2442e6adff68b8ab5d06d3b6228dad7af8a
Brett Sutton <bsutton@onepub.dev>
2026-08-19 16:01:19 +1000

    Use absolute Dart conformance executable path

--- bindings/e2e/dart/package_conformance.dart
+++ bindings/e2e/dart/package_conformance.dart
@@ -20,7 +20,7 @@ Future<void> main(List<String> arguments) async {
   final install = Directory('${work.path}/dart-install');
   await install.create(recursive: true);
   final executable = File(
-    '${install.path}/bundle/bin/conformance${Platform.isWindows ? '.exe' : ''}',
+    '${install.absolute.path}/bundle/bin/conformance${Platform.isWindows ? '.exe' : ''}',
   );
 
   await _run(
